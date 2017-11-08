fa599cf15f3fd03de00b69a007e039c00ddf6fd6
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2017-11-08 20:37:58 +0545

    Disallow registraion of multiple users wuth same login

--- server/src/auth/register.js
+++ server/src/auth/register.js
@@ -8,10 +8,17 @@ export default (app) => {
     const {login, password, passwordRepeat} = req.body;
 
     if (password !== passwordRepeat) {
-      return res.status(400).send({error: 'Passowrd do not match!'});
+      return res.status(400).send({error: 'Password do not match!'});
     }
 
+    // hash password
     const hashedPassword = hash(password);
+
+    // check if login already taken
+    const users = await User.filter({login}).run();
+    if (users.length > 0) {
+      return res.status(403).send({error: 'User already exists!'});
+    }
 
     const user = new User({
       login,
