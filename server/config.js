3343be8de3532df5f1b01f9c73f27ef5798f5924
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2017-11-14 06:02:03 +0545

    User ES5 syntax for config, allow overriding config value with env variables

--- server/config.js
+++ server/config.js
@@ -1,11 +1,14 @@
-export const db = {
-  host: 'localhost',
-  port: 28015,
+exports.db = {
+  host: process.env.EXPERTS_DB_URL || 'localhost',
+  port: process.env.EXPERTS_DB_PORT || 28015,
   db: 'expertsdb',
 };
 
-export const auth = {
-  passwordSalt: 'UIqG34oNQDoT3BYKpJCVngv4wM2OihkaJaE@6zV!qf8C&RV&t2Gr2PquIV*CQZ7O',
-  sessionSecret: 'rSshR4iClKnk0G45k@sBhx3!rbu0Z8lasGR38goRy8m!A#Yiuw#tEAP1OM2t@5Bi',
-  jwtSecret: 'hlQfxVALKFgx2Xtq7eK#iX3kkNhoA$f3f@04G4ci90yV&JVsWgAjba76H3fo&Kfr',
+exports.auth = {
+  passwordSalt: process.env.EXPERTS_AUTH_PASSALT ||
+    'UIqG34oNQDoT3BYKpJCVngv4wM2OihkaJaE@6zV!qf8C&RV&t2Gr2PquIV*CQZ7O',
+  sessionSecret: process.env.EXPERTS_AUTH_SESSECRET ||
+    'rSshR4iClKnk0G45k@sBhx3!rbu0Z8lasGR38goRy8m!A#Yiuw#tEAP1OM2t@5Bi',
+  jwtSecret: process.env.EXPERTS_AUTH_JSWSECRET ||
+    'hlQfxVALKFgx2Xtq7eK#iX3kkNhoA$f3f@04G4ci90yV&JVsWgAjba76H3fo&Kfr',
 };
