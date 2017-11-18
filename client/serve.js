5ea19b4f30500eec2b0f9b33b791c1c9c2982ab9
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2017-11-18 17:33:32 +0545

    Add debug server script and use it for npm start

---
+++ client/serve.js
@@ -0,0 +1,52 @@
+/* eslint no-console: 0 */
+// start webpack
+const path = require('path');
+const express = require('express');
+const webpack = require('webpack');
+const webpackMiddlware = require('webpack-dev-middleware');
+const webpackHotMiddlware = require('webpack-hot-middleware');
+const config = require('./webpack.config');
+
+// create express
+const app = express();
+
+// setup hot reload
+config.plugins.push(new webpack.HotModuleReplacementPlugin());
+// setup no errors plugin
+config.plugins.push(new webpack.NoEmitOnErrorsPlugin());
+// override entry for hotload
+config.entry = [
+  'webpack-hot-middleware/client',
+  config.entry,
+];
+// returns a Compiler instance
+const compiler = webpack(config);
+// stats output config
+const statsConf = {
+  colors: true,
+  hash: false,
+  timings: true,
+  chunks: false,
+  chunkModules: false,
+  modules: false,
+};
+app.use(webpackMiddlware(compiler, {
+  publicPath: config.output.publicPath,
+  contentBase: 'src',
+  stats: statsConf,
+}));
+app.use(webpackHotMiddlware(compiler));
+
+// serve static
+app.use(express.static(__dirname));
+
+// server index
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+// start server
+app.listen(3000, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.info('==> Listening on port 3000');
+  }
+});
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => (
-  <h1>Hello, world</h1>
+  <h1>Hello, world!</h1>
 );
 
 const div = document.getElementById('app');
