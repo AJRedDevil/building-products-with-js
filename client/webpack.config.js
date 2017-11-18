1cc766b16516ad2dc2a33d563b5df7c411b5d902
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2017-11-18 16:52:54 +0545

    Set up basic client project with webpack, babel and react

---
+++ client/webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  devtool: 'cheap-module-source-map',
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      debug: true,
+    }),
+  ],
+  context: path.resolve(__dirname, 'src'),
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',
+    filename: 'app.min.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style',
+          'css?modules',
+        ],
+        exclude: /node_modules/,
+      }, {
+        test: /node_modules\/.+\.css$/,
+        use: [
+          'style',
+          'css',
+        ],
+      }, {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            query: {
+              cacheDirectory: true,
+              presets: ['env', 'react', 'stage-0'],
+              plugins: ['transform-runtime'],
+              env: {
+                development: {
+                  presets: ['react-hmre'],
+                },
+                production: {
+                  presets: ['react-optimize'],
+                },
+              },
+            },
+          },
+        ],
+      }, {
+        test: /\.json$/,
+        loader: 'json',
+      }, {
+        test: /\.woff\d?(\?.+)?$/,
+        loader: 'url?limit=10000&mimetype=application/font-woff',
+      }, {
+        test: /\.ttf(\?.+)?$/,
+        loader: 'url?limit=10000&mimetype=application/octet-stream',
+      }, {
+        test: /\.eot(\?.+)?$/,
+        loader: 'url?limit=10000',
+      }, {
+        test: /\.svg(\?.+)?$/,
+        loader: 'url?limit=10000&mimetype=image/svg+xml',
+      }, {
+        test: /\.png$/,
+        loader: 'url?limit=10000&mimetype=image/png',
+      }, {
+        test: /\.gif$/,
+        loader: 'url?limit=10000&mimetype=image/gif',
+      },
+    ],
+  },
+};
