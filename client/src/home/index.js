e76fd90601cd942de61eb4c8914f29462aa68144
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2017-11-19 07:57:34 +0545

    Make home a stateful component to make hotreload demo work

--- client/src/home/index.js
+++ client/src/home/index.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
-export default () => (
-  <div>
-    <h1>hello, world!</h1>
-    <Link to="/other">other</Link>
-  </div>
-);
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {world: 'world'};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, {this.state.world}!</h1>
+        <Link to="/other">other</Link>
+      </div>
+    );
+  }
+}
